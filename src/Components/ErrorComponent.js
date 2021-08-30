3fb5abfbd5dcbc31ecdd292cbf29b61bdf807845
Sumit Kumar <sumitstark1994@gmail.com>
2021-08-30 20:57:02 +0100

    added componentDidCatch

--- src/Components/ErrorComponent.js
+++ src/Components/ErrorComponent.js
@@ -1,15 +1,21 @@
 import React from 'react';
+class ErrorComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentDidCatch(error, errorInfo) {
+        console.log('Error', error)
+        console.log('Error', error)
+        this.setState({
+            error: error,
+            errorInfo: errorInfo
+        })
+    }
 
-const ErrorComponent = () => {
-    return (
-        <div className='ui fixed menu'>
-            <div className='ui container center'>
-                <h2 style={{ display: 'flex' }}> No asset data found </h2>
-              
-            </div>
-
-        </div>
-    );
+    render() {
+        // You can render any custom fallback UI
+        return (<h1 style={{ display: 'flex' }}> No asset data found </h1>)
+    }
 }
 
 export default ErrorComponent;
--- src/Components/__test__/ErrorComponent.test.js
+++ src/Components/__test__/ErrorComponent.test.js
@@ -1,13 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import ErrorComponent from '../ErrorComponent';
+const ChildComponent= () => {throw new Error()};
 
-it('Should renders header text', () => {
-  render(<ErrorComponent />);
+
+it('Should renders error message', () => {
+  render(<ErrorComponent > <ChildComponent/></ErrorComponent>);
   const headingElement = screen.getByText(/No asset data found/i);
   expect(headingElement).toBeInTheDocument();
 });
-it('Should renders heading', () => {
-    render(<ErrorComponent />);
-    const headingElement = screen.getByRole('heading');
+
+it('Should renders header text', () => {
+    render(<ErrorComponent > <ChildComponent/></ErrorComponent>);
+    const headingElement = screen.getByText(/No asset data found/i);
     expect(headingElement).toBeInTheDocument();
   });
+
+
