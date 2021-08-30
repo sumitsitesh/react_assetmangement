0ce98bb80f6832b37fae731f30daae7b5f22b206
Sumit Kumar <sumitstark1994@gmail.com>
2021-08-30 05:09:48 +0100

    assetdata change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import Header from './Components/Header'
 import AssetTable from './Components/AssetTable';
-import ErrorComponent from './Components/ErrorComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <ErrorComponent>
       <AssetTable />
-      </ErrorComponent>
-      
     </div>
   );
 }
